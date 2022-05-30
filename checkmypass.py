3c787fe1aa9f5579d6ff88bc7f5add9c8ee7af1a
RonaldVilorio <ronvilorio@gmail.com>
2022-05-30 15:37:47 -0400

    password to sha1 method and request api method added

--- checkmypass.py
+++ checkmypass.py
@@ -1,2 +1,18 @@
 import requests
+import hashlib
 
+
+def request_api_data(query_char):
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f"Error fetching: {res.status_code}")
+    return res
+
+
+def pwned_api_check(password):
+    sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    return sha1_password
+
+# request_api_data("123")
+pwned_api_check("hello")
