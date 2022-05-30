8cceffd24c4b0ef3b7c9cd3de3367ddea547d97a
RonaldVilorio <ronvilorio@gmail.com>
2022-05-30 16:47:27 -0400

    getpasswordleaks matches hatch and returns number of times pass was breached

--- checkmypass.py
+++ checkmypass.py
@@ -8,13 +8,20 @@ def request_api_data(query_char):
     if res.status_code != 200:
         raise RuntimeError(f"Error fetching: {res.status_code}")
     return res
-
-
+    
+def get_password_leaks_count(hashes,hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for hash,count in hashes:
+        if hash == hash_to_check:
+            return (hash,count)
+        else:
+            continue
+        
 def pwned_api_check(password):
     sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first5char, tail = sha1_password[:5], sha1_password[5:]
     response = request_api_data(first5char)
-    print(response)
+    return get_password_leaks_count(response,tail)
 
 # request_api_data("123")
-pwned_api_check("hello")
+print(pwned_api_check("hello"))
